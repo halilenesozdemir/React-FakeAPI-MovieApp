566a62e6fc8c9ed4a22c4f87eb2dedcb1884b1c9
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-05-08 23:55:04 +0300

    React-Fake API- Arin

---
+++ src/components/SearchBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  handleFormSubmit = e => {
+    e.preventDefault();
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <div className="form-row mt-3 mb-5">
+          <div className="col-12">
+            <input
+              onChange={this.props.searchMovieProp}
+              type="text"
+              className="form-control"
+              placeholder="Search a movie" // Değeri React tarafından kontrol edilebilen form elemanı ( Controlled component )
+            />
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
